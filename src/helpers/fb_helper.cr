d3552e26fb24e41cc43db1b6157cd8d12f2d3c6e
Tatsiujin Chin <c910335@gmail.com>
2018-09-02 11:39:39 +0800

    implement fb user

--- src/helpers/fb_helper.cr
+++ src/helpers/fb_helper.cr
@@ -7,34 +7,47 @@ module FBHelper
   @token_info : TokenInfo?
   @token_string : String?
   @token_scopes : Array(String)?
+  @fb_user : FBUser?
 
   forward_missing_to FBHelper
 
-  def self.config(key : String)
+  def self.config(key : String) : String
     Amber.settings.secrets.try(&.[key]).not_nil!
   end
 
-  def self.base_url
+  def self.base_url : String
     @@base_url ||= config "fb_base_url"
   end
 
-  def self.client_token
+  def self.client_token : String
     @@client_token ||= config "fb_client_token"
   end
 
-  def fb_authenticate!
-    return token_missing!(REALM) && nil unless token_string
-    return token_invalid!(REALM) && nil unless token_info? && token_info.data.is_valid
-    return scope_insufficient!(REALM) && nil unless (token_scopes & SCOPES).size == SCOPES.size
-    token_info.data
+  def fb_authenticate! : TokenInfo::Data | Nil
+    token_missing!(REALM) || return unless token_string
+    token_invalid!(REALM) || return unless token_info? && token_data.is_valid
+    scope_insufficient!(REALM) || return unless (token_scopes & SCOPES).size == SCOPES.size
+    token_data
   end
 
-  def token_info : TokenInfo
-    token_info?.not_nil!
+  def fb_user : FBUser
+    fb_user?.not_nil!
+  end
+
+  def fb_user? : FBUser | Nil
+    @fb_user ||= HTTP::Client.get("#{base_url}/v3.1/me?fields=name,email", headers: HTTP::Headers{"Authorization" => "Bearer #{token_string}"}) do |response|
+      if response.success?
+        FBUser.from_json(response.body_io)
+      end
+    end
   end
 
   def token_scopes : Array(String)
-    @token_scopes ||= token_info.data.scopes
+    @token_scopes ||= token_data.scopes
+  end
+
+  def token_data : TokenInfo::Data
+    token_info?.not_nil!.data
   end
 
   def token_info? : TokenInfo | Nil
@@ -47,6 +60,14 @@ module FBHelper
 
   def token_string : String | Nil
     @token_string ||= (authorization = request.headers["Authorization"]?) && authorization[/Bearer (.*)$/, 1]?
+  end
+
+  class FBUser
+    JSON.mapping(
+      id: String,
+      name: String,
+      email: String
+    )
   end
 
   class TokenInfo
